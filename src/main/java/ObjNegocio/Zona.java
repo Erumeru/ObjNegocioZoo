d3b79cf349183d97515b6152cea970b9f2633c3a
Marki <124947044+MarkiHub@users.noreply.github.com>
2023-05-16 11:47:58 -0700

    fix: equals

--- src/main/java/ObjNegocio/Zona.java
+++ src/main/java/ObjNegocio/Zona.java
@@ -4,6 +4,7 @@
  */
 package ObjNegocio;
 
+import java.util.Objects;
 import org.bson.types.ObjectId;
 
 /**
@@ -44,6 +45,28 @@ public class Zona {
 
     public void setId(ObjectId id) {
         this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Zona other = (Zona) obj;
+        return Objects.equals(this.id, other.id);
     }
     
     
