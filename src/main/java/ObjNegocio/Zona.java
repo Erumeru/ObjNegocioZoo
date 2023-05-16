b1c6c66f256a0248a2de3be491d7fdb544bc70e1
Erumeru <elpichikoala@gmail.com>
2023-05-15 23:41:52 -0700

    Se agregaron las zonas

--- src/main/java/ObjNegocio/Zona.java
+++ src/main/java/ObjNegocio/Zona.java
@@ -4,10 +4,47 @@
  */
 package ObjNegocio;
 
+import org.bson.types.ObjectId;
+
 /**
  *
  * @author eruma
  */
 public class Zona {
+    
+    private float extension;
+    private String nombre;
+    private ObjectId id;
+
+    public Zona(float longitud, String nombre, ObjectId id) {
+        this.longitud = longitud;
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public float getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(float longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+    
     
 }
